2dc66fb875b91c63f9404e3cf196e53c6ed651d0
JCPenne <jackcpalmer@icloud.com>
2023-02-19 02:20:00 -0800

    manually copy mujeeb files

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../styles.css';
 import { useNavigate } from 'react-router-dom';
-import background from '../../Images/background-2.jpg';
 
 //We need a fullscreen background, A title, An intro blurb, A play button
 
@@ -9,9 +8,19 @@ function LandingPage() {
   const navigate = useNavigate();
   return (
     <div>
-      <div className='start-story'>
-        Story line starts with fade in fade out
+      <div className='fade-in-and-fade-out'>
+        Game description: This is a modified word scrambling game. The
+        objective is to rearrange the jumbled words before the time
+        runs out.
       </div>
+      <div className='fade-in'>
+        Two friends, rabbit and snake, hike up a mountain. When they
+        reach the top, the snake disappears. Rabbit feels scared
+        without the snake. A ghost appears and shouts at rabbit. Now,
+        you must translate these words to continue your journey to
+        safety.
+      </div>
+      <div className='container'> </div>
       {/* If storyline has finished telling, then show the instruction paragraph and button */}
       <button
         className='Button'
