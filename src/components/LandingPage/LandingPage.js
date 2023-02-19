850763c7901f0920ebc1bf07c965c3a2270b96b2
Mujeeb-web <mujeebisnot@hotmail.com>
2023-02-18 23:22:42 -0800

    styling buttons

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../styles.css';
 import { useNavigate } from 'react-router-dom';
 
 //We need a fullscreen background, A title, An intro blurb, A play button
@@ -7,9 +8,9 @@ function LandingPage() {
   const navigate = useNavigate();
   return (
     <div>
-      <div>Story line starts with fade in fade out</div>
+      <div className='fade-in-out'>Story line starts with fade in fade out</div>
       {/* If storyline has finished telling, then show the instruction paragraph and button */}
-      <button onClick={() => navigate('/game')}>Start</button>
+      <button className='Button' onClick={() => navigate('/game')}>Start</button>
     </div>
   );
 }
