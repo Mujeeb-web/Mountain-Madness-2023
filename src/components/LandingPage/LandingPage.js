3a63c23e65429a6637fa943a9def2bee0dac6c81
JCPenne <jackcpalmer@icloud.com>
2023-02-18 17:50:49 -0800

    initial commit

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+//We need a fullscreen background, A title, An intro blurb, A play button
+
+function LandingPage() {
+  return (
+    <div>
+      <title>Ghost Babble</title>
+      <p className='title'>Ghost Babble: The Mountain Escape</p>
+      <section className='blurb'>
+        lorem ipsum sdflkj sflksdjf sdfsdf ljkl sdfdsf lkjkljs fsfd
+        lkjlkj
+      </section>
+      <button>Play!</button>
+    </div>
+  );
+}
+
+export default LandingPage;
